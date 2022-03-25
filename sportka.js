136739784858dab1f3299290879404c40344685a
Zuzana Volfová <zuzka.volfova@gmail.com>
2022-03-25 17:47:41 +0100

    add button, style button

--- sportka.js
+++ sportka.js
@@ -5,20 +5,22 @@ let vyherniCisla = document.querySelector('#vyherni-cisla');
 let tazenaCisla = [];
 
 //naplním osudí
-for (let i = 1; i <= 49; i = i + 1) {
-    osudi.push(i);
-}
+function sportka() {
+    for (let i = 1; i <= 49; i = i + 1) {
+        osudi.push(i);
+    }
 
-// z osudi vyberu 7 náhodných čísel
-for (let i = 0; i < 7; i++) {
-    let vyherniIndex = Math.floor(Math.random() * osudi.length);
-    let vyherniCislo = osudi[vyherniIndex];
+    // z osudi vyberu 7 náhodných čísel
+    for (let i = 0; i < 7; i++) {
+        let vyherniIndex = Math.floor(Math.random() * osudi.length);
+        let vyherniCislo = osudi[vyherniIndex];
 
-    tazenaCisla.push(vyherniCislo);
-    osudi.splice(vyherniIndex, 1);
-}
+        tazenaCisla.push(vyherniCislo);
+        osudi.splice(vyherniIndex, 1);
+    }
 
-//vypíšu do HTML
-for (let i = 0; i < tazenaCisla.length; i++) {
-    vyherniCisla.innerHTML += '<span class="cislo">' +  tazenaCisla[i] + '</span>';
+    //vypíšu do HTML
+    for (let i = 0; i < tazenaCisla.length; i++) {
+        vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[i] + '</span>';
+    }
 }
